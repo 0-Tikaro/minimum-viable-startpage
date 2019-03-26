44e3ecfbeec122206cdfb86ca91b5fa6144b90b9
Carl Mercier <foss@carlmercier.com>
2019-03-26 15:16:26 -0400

    Allow random background image and item dividers

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
-const NAME = "John";
+const NAME = "Carl";
 const WELCOME_MESSAGE_TEMPLATE = ["night", "morning", "afternoon", "evening"];
 
-// All shortcuts are in a `SHORTCUT_STARTER+shortcutKey` format. 
+// All shortcuts are in a `SHORTCUT_STARTER+shortcutKey` format.
 // So, for example, pressing `tab+q` would redirect you to https://google.com/?q=q
-const SHORTCUT_STARTER = 'tab' 
+const SHORTCUT_STARTER = 'tab'
 
 // How much time (in milliseconds) you have to press shortcutKey after pressing SHORTCUT_STARTER.
 // Also change --SHORTCUT_TIMEOUT in styles.css if you change this option.
 const SHORTCUT_TIMEOUT = 1500;
+
+// A list of background images to be displayed at random. Find images you like at: https://source.unsplash.com/random/1920×1080
+const BACKGROUND_IMAGES = [
+    "https://images.unsplash.com/photo-1551634039-774fb4acf042?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    "https://images.unsplash.com/photo-1551432434-d5f2bdc62ce9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    "https://images.unsplash.com/photo-1552400778-ef424af551f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    "https://images.pexels.com/photos/1832328/pexels-photo-1832328.jpeg?cs=srgb&dl=adventure-agriculture-asia-1832328.jpg&fm=jpg?dl&fit=crop&crop=entropy&w=1920&h=1280",
+    "assets/bg.jpg"
+]
 
 // The groups of links are generated from this object. Edit it to edit the page's contents.
 // shortcutKey must hold an all-lowercase single button. Theoretically should work with values like `esc` and `f1`,
 // but intended to be used with just regular latin letters.
 const MASTER_MAP = [
     {
-        "groupName": "Studies",
+        "groupName": "Stock & Crypto",
         "items":[
-            {"name": "Item A", "shortcutKey": "q", "url": "https://google.com/?q=q"},
-            {"name": "Item B", "shortcutKey": "w", "url": "https://google.com/?q=w"},
-            {"name": "Item C", "shortcutKey": "e", "url": "https://google.com/?q=e"}
+            {"name": "WSJ", "shortcutKey": "w", "url": "https://wsj.com"},
+            {"name": "CNBC", "shortcutKey": "c", "url": "https://cnbc.com"},
+            {"name": "Sharesight", "shortcutKey": "e", "url": "https://sharesight.com"},
+            { "divider": true },
+            { "name": "CoinMarketCap", "shortcutKey": "a", "url": "https://coinmarketcap.com" },
+            { "name": "CoinTracking.info", "shortcutKey": "s", "url": "https://cointracking.info" },
+
         ]
     },
     {
-        "groupName": "Work",
+        "groupName": "Lab",
         "items":[
-            {"name": "Item D", "shortcutKey": "a", "url": "https://google.com/?q=a"},
-            {"name": "Item E", "shortcutKey": "s", "url": "https://google.com/?q=s"},
-            {"name": "Item F", "shortcutKey": "d", "url": "https://google.com/?q=d"}
+            { "name": "HomeAssistant", "shortcutKey": "h", "url": "http://hass.local:8123/"},
+            { "name": "pfSense", "shortcutKey": "x", "url": "http://10.0.0.1/"},
+            { "name": "Proxmox", "shortcutKey": "x", "url": "https://proxmox.piggerbok.com/"},
+            { "name": "Rancher", "shortcutKey": "x", "url": "https://rancher.piggerbok.com/" },
+            { "name": "CloudCommander", "shortcutKey": "x", "url": "http://10.0.0.4:8765/" }
         ]
     },
     {
+        "groupName": "Media",
+        "items": [
+            { "name": "Ombi", "shortcutKey": "o", "url": "https://home.piggerbok.com/ombi" },
+            { "name": "Sonarr", "shortcutKey": "t", "url": "https://home.piggerbok.com/sonarr" },
+            { "name": "Radarr", "shortcutKey": "m", "url": "https://home.piggerbok.com/radarr" },
+            { "name": "SABnzbd", "shortcutKey": "s", "url": "https://home.piggerbok.com/sabnzbd" },
         "groupName": "Personal",
         "items":[
             {"name": "Item I", "shortcutKey": "z", "url": "https://google.com/?q=z"},
             {"name": "Item J", "shortcutKey": "x", "url": "https://google.com/?q=x"},
             {"name": "Item K", "shortcutKey": "c", "url": "https://google.com/?q=c"}
+
+
         ]
     }
+
 ]
 
 let $container = document.getElementById("content");
@@ -45,6 +69,12 @@ let getUrl = {};
 let $shortcutDisplayList = document.getElementsByClassName("shortcut");
 let listeningForShortcut = false;
 let listenerTimeout;
+
+function setBackgroundImage() {
+    let imgUrl = BACKGROUND_IMAGES[Math.floor(Math.random() * BACKGROUND_IMAGES.length)];
+    let beforeContainer = document.getElementById('beforeContainer');
+    beforeContainer.style.backgroundImage = "url('" + imgUrl + "')";
+}
 
 function setupWelcomeMessage(){
     let curHours = new Date().getHours();
@@ -72,18 +102,23 @@ function setupGroups(){
             let pContainer = document.createElement("p");
             group.appendChild(pContainer);
 
-            let link = document.createElement("a");
-            link.innerHTML = curItemData.name;
-            link.setAttribute("href", curItemData.url);
-            pContainer.appendChild(link);
+            if (curItemData.divider == true) {
+                let hr = document.createElement("hr");
+                pContainer.appendChild(hr);
+            } else {
+                let link = document.createElement("a");
+                link.innerHTML = curItemData.name;
+                link.setAttribute("href", curItemData.url);
+                pContainer.appendChild(link);
 
-            let shortcutDisplay = document.createElement("span");
-            shortcutDisplay.innerHTML = curItemData.shortcutKey;
-            shortcutDisplay.className = "shortcut";
-            shortcutDisplay.style.animation = "none";
-            pContainer.appendChild(shortcutDisplay);
+                let shortcutDisplay = document.createElement("span");
+                shortcutDisplay.innerHTML = curItemData.shortcutKey;
+                shortcutDisplay.className = "shortcut";
+                shortcutDisplay.style.animation = "none";
+                pContainer.appendChild(shortcutDisplay);
 
-            getUrl[curItemData.shortcutKey] = curItemData.url
+                getUrl[curItemData.shortcutKey] = curItemData.url
+            }
         }
     }
 }
@@ -110,6 +145,7 @@ function shortcutListener(e) {
 }
 
 function main(){
+    setBackgroundImage();
     setupWelcomeMessage();
     setupGroups();
     document.addEventListener('keyup', shortcutListener, false);
